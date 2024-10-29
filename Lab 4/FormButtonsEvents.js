4e5640ad5d67096643667ac2e46881a5879d23b9
AtliTyr <akulibabaev2.0@mail.ru>
2024-10-30 01:47:58 +0300

    ESLint integration

--- Lab 4/FormButtonsEvents.js
+++ Lab 4/FormButtonsEvents.js
@@ -1,30 +1,29 @@
-let elem = document.getElementsByClassName("submit_button").item(0);
-elem.addEventListener("click", fillHiddenInputs);
-
-function fillHiddenInputs()
-{
+function fillHiddenInputs() {
     let elems = document.querySelectorAll(".order_update > div > input");
 
-    if(selectedSoup) {
+    if (selectedSoup) {
         let obj = searchByKeyword(selectedSoup.dataset.dish);
         elems.item(0).value = `${obj.keyword}`;
     } else {
         elems.item(0).value = ``;
     }
-    if(selectedMain_course) {
+    if (selectedMain_course) {
         let obj = searchByKeyword(selectedMain_course.dataset.dish);
         elems.item(1).value = `${obj.keyword}`;
     } else {
         elems.item(1).value = ``;
     }
-    if(selectedBeverages) {
+    if (selectedBeverages) {
         let obj = searchByKeyword(selectedBeverages.dataset.dish);
         elems.item(2).value = `${obj.keyword}`;
     } else {
         elems.item(2).value = ``;
     }
-    if(total)
+    if (total)
         elems.item(3).value = total;
     else
         elems.item(3).value = ``;
 }
+
+let elem = document.getElementsByClassName("submit_button").item(0);
+elem.addEventListener("click", fillHiddenInputs);
--- Lab 4/validateForm.js
+++ Lab 4/validateForm.js
@@ -1,7 +1,8 @@
 document.forms.item(0).addEventListener("submit", function(event) {
-        if(selectedBeverages === undefined || selectedSoup === undefined || selectedMain_course === undefined) 
-        { 
-            event.preventDefault();
-            alert('Заказ не полный');
-        }   
-    });
+    if (selectedBeverages === undefined || 
+        selectedSoup === undefined || 
+        selectedMain_course === undefined) { 
+        event.preventDefault();
+        alert('Заказ не полный');
+    }   
+});
