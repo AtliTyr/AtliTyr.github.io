1d74b93feba909ecf64d1b2061f1174799752328
AtliTyr <akulibabaev2.0@mail.ru>
2025-03-10 18:25:22 +0300

    fixing ve

--- Web Dev App/Lab 1/ve/Scripts/activate.bat
+++ Web Dev App/Lab 1/ve/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ACER\Desktop\УЧЕНИЕ\Разработка веб-приложений\Лаб 1\ve
+set VIRTUAL_ENV=C:\Users\ACER\Desktop\УЧЕНИЕ\Разработка веб-приложений\AtliTyr.github.io\Web Dev App\Lab 1\ve
 
 if not defined PROMPT set PROMPT=$P$G
 
