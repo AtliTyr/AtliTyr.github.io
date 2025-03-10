898f522ffae66a67e3343d47924aefd3c8555ab8
AtliTyr <48732012+AtliTyr@users.noreply.github.com>
2025-03-10 17:40:25 +0300

    Delete Exam directory

--- Exam/mainPage/mainPageSortTypeScript.js
+++
@@ -1,8 +0,0 @@
-let sortType = "rating-desc";
-
-document.querySelector(`.header-of-catalog select`).
-    addEventListener("change", function (event) {
-        sortType = event.currentTarget.value;
-        deleteMainContent();
-        displayMainContent();
-    });
