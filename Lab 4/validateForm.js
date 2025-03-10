0606e85bc4e4905b912f052c04cebb3af91b7db4
AtliTyr <48732012+AtliTyr@users.noreply.github.com>
2025-03-10 17:41:45 +0300

    Delete Lab 4 directory

--- Lab 4/validateForm.js
+++
@@ -1,8 +0,0 @@
-document.forms.item(0).addEventListener("submit", function(event) {
-    if (selectedBeverages === undefined || 
-        selectedSoup === undefined || 
-        selectedMain_course === undefined) { 
-        event.preventDefault();
-        alert('Заказ не полный');
-    }   
-});
